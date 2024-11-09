1ee0fb7440c5b5445b9ecfc9f5a8b9606dc6d48f
Rahul Biradar <126542436+rahulbiradar06@users.noreply.github.com>
2024-11-10 04:54:03 +0530

    changed theme in content one and two and added image

--- eco-advisor/src/components/landingpage/ContentOne.js
+++ eco-advisor/src/components/landingpage/ContentOne.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const ContentOne = () => {
   return (
-    <section style={{ backgroundColor: '#c7fff3' }}>
+    <section >
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
           <h2 className="mb-4 text-4xl tracking-tight font-bold text-gray-900 dark:text-white">
