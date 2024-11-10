a6780bcd537a5ecd671dd3b3b34bcf8e494defe1
DJ0301 <dhananjayjoshi2003@gmail.com>
2024-11-10 12:03:54 +0530

    added heading animations

--- eco-advisor/src/components/landingpage/Herotwo.js
+++ eco-advisor/src/components/landingpage/Herotwo.js
@@ -10,7 +10,7 @@ export const Herotwo = () => {
       </div>
       <div className="herotwo-half plant">
         <div className="mx-auto max-w-screen-xl text-center lg:px-12">
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none herotwo-text-gradient md:text-5xl lg:text-6xl">
             Your partner in sustainable living 
           </h1>
           <p className="mb-8 text-lg font-normal text-gray-900 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
